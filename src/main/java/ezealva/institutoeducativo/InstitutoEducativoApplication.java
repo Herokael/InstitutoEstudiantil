539b18f092468806b0649564b80f32d4cc13151d
Ezequiel Alvarez <ezequielb.perez@gmail.com>
2023-09-01 11:45:27 -0300

    Spring Boot configuration

---
+++ src/main/java/ezealva/institutoeducativo/InstitutoEducativoApplication.java
@@ -0,0 +1,13 @@
+package ezealva.institutoeducativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutoEducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutoEducativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ezealva/institutoeducativo/InstitutoEducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package ezealva.institutoeducativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoEducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
